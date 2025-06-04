78554d14389dd45c3ef737741241fd27d32b0834
Tomás Fava <tomsfava@gmail.com>
2025-06-04 12:45:04 -0300

    resolvendo testes locais adicionando opções a .environ.get()

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -143,7 +143,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "foo")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
