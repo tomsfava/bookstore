18c10c56090c276fb35f1555b0f34a59d68f857d
Tomás Fava <tomsfava@gmail.com>
2025-06-04 16:56:06 -0300

    pronto para o deploy?

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -71,6 +72,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Database
@@ -145,6 +150,7 @@ INTERNAL_IPS = [
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "foo")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tomsf-bookstore-api-d71ea6a108db.herokuapp.com/' ]
+
